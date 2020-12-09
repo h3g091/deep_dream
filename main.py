f4220c2b672bfb4259a61a102afdcf4ea9becf95
h3g091 <heikolanger91@gmx.de>
2020-12-09 17:03:30 +0100

    added main.py data

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  9 16:53:19 2020
+
+@author: heiko.langer
+"""
+
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import random
+import math
+# Image manipulation.
+import PIL.Image
+from scipy.ndimage.filters import gaussian_filter
+import inception5h
