30181814a5f995bdbc523f3e1aed68ca32ff19b3
h3g091 <heikolanger91@gmx.de>
2021-03-22 09:42:11 +0100

    added new pic for dreaming. hyperparameter tuning get rid of loop for speed up#

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ start = timeit.default_timer()
 
 #Your statements here
 
-layer_tensor = model.layer_tensors[3]
-for iteration in range(10):
+layer_tensor = model.layer_tensors[2]
+for iteration in range(1):
     #iteration=0
-    file_name = "/Users/heiko.langer/deep_dream_fun/picture_" + str(iteration).zfill(3)+'.jpg'
+    #file_name = "/Users/heiko.langer/deep_dream_fun/picture_" + str(iteration).zfill(3)+'.jpg'
+    file_name = "/Users/heiko.langer/deep_dream_fun/tassilo_pic/hound.jpg"
 #file_name = "the-starry-night/the-starry-night-800x450.jpg"
     img = load_image(filename='{}'.format(file_name))
     #layer_tensor = model.layer_tensors[iteration]
@@ -18,16 +19,17 @@ for iteration in range(10):
     
     img_result = recursive_optimize(layer_tensor=layer_tensor, image=img,
                      # how clear is the dream vs original image
-                     num_iterations=1, step_size=0.1, rescale_factor=0.5,
+                     num_iterations=40, step_size=0.5, rescale_factor=0.5,
+                     # step size bigger for cra
                                     #num_iterations=20, step_size=1.0, rescale_factor=0.5,
                      # How many "passes" over the data. More passes, the more granular the gradients will be.
-                     num_repeats=8, blend=0.2)
+                     num_repeats=4, blend=0.2)
     
     img_result = np.clip(img_result, 0.0, 255.0)
     img_result = img_result.astype(np.uint8)
     result = PIL.Image.fromarray(img_result, mode='RGB')
   
-    result.save('picture_'+str(iteration+1).zfill(3)+'.jpg')
+    result.save('tassilo_15'+str(iteration+1).zfill(3)+'.jpg')
 #    result.show()
 
     
@@ -35,5 +37,5 @@ for iteration in range(10):
     
 stop = timeit.default_timer()
 print('Time: ', stop - start)  
-
+print('Time: ', (stop - start)/60)  
 
